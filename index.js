f900d7014dbb5e3c1cabe0b53c071f6256363ab9
Gabriel Moreno Cavalcanti <38257470+GabrielMVP@users.noreply.github.com>
2023-11-02 10:15:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let numA = 4;
+
+let numB = 5;
+
+let soma = `${numA} + ${numB} = ${numA + numB}`
+
+let subtracao = `${numA} - ${numB} = ${numA - numB}`
+
+let multiplicacao =  `${numA} x ${numB} = ${numA * numB}`
+
+let divisao = `${numA} / ${numB} = ${numA / numB}`
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
